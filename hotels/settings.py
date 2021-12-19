ae897d8776340208e3d3a2b8172b7e260de8804e
FaryalBhatti <71561187+FaryalBhatti@users.noreply.github.com>
2021-12-19 20:48:04 +0500

    Update settings.py

--- hotels/settings.py
+++ hotels/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'as!c#xo36b8c6fbcm(u&ks(54f@cd#r&11nd971q#k28d*22mi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hsearching.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000','hsearching.herokuapp.com']
 
 
 # Application definition
